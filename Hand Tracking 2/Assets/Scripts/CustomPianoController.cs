3860e3af39d2159c2b2c4268e37a6635163d618c
DarkforcesII <35416278+DarkforcesII@users.noreply.github.com>
2022-01-05 16:12:56 -0800

    Refactored Code

---
+++ Hand Tracking 2/Assets/Scripts/CustomPianoController.cs
@@ -0,0 +1,253 @@
+using OculusSampleFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UniOSC;
+
+public class CustomPianoController : MonoBehaviour
+{
+    [SerializeField] 
+    private CustomAudioManager _audioManger = null;
+
+    private bool limitSelection = false;
+
+    [SerializeField]
+    private UniOSCEventDispatcherButton dispatcher;
+
+    void Awake()
+    {
+        Assert.IsNotNull(_audioManger);
+    }
+
+    // coroutine to reset bool and limit selection
+    #region
+    private IEnumerator ResetBool()
+    {
+        yield return new WaitForSeconds(1);
+        limitSelection = false;
+    }
+    #endregion
+
+    // octave 3
+    #region
+    public void PlayNoteC3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayC3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+    public void PlayNoteCSharp3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayCSharp3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteD3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayD3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteDSharp3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayDSharp3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteE3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayE3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteF3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayF3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteFSharp3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayFSharp3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteG3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayG3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteGSharp3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayGSharp3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteA3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayA3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteBb3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayBb3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteB3(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayB3Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+    #endregion
+
+    // octave 4
+    #region
+    public void PlayNoteC4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayC4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+    public void PlayNoteCSharp4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayCSharp4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteD4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayD4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteDSharp4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayDSharp4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteE4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayE4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteF4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayF4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteFSharp4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayFSharp4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteG4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayG4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteGSharp4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayGSharp4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteA4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayA4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteBb4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayBb4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    public void PlayNoteB4(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            _audioManger.PlayB4Key();
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+
+    #endregion
+}
--- Hand Tracking 2/Assets/Scripts/CustomUIController.cs
+++ Hand Tracking 2/Assets/Scripts/CustomUIController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using OculusSampleFramework;
+using UniOSC;
 
 public class CustomUIController : MonoBehaviour
 {
+    // limits how often a button can be selected while colliding with it
     private bool limitSelection = false;
 
     public Transform piano;
@@ -13,14 +15,25 @@ public class CustomUIController : MonoBehaviour
     [SerializeField]
     private float scale;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    // references to text and UniOSC classes
+    #region
+    public Text ipAddress;
 
-    }
+    [SerializeField]
+    private UniOSCConnection ipConnection;
+
+    [SerializeField]
+    private UniOSCEventDispatcherButton dispatcher;
+
+    private string fillerText = "";
+
+    // for testing
+    public InputField textField;
+    #endregion
 
     private void Update()
     {
+        // for testing
         if (Input.GetKeyDown(KeyCode.A))
         {
             piano.localScale *= scale;
@@ -30,8 +43,231 @@ public class CustomUIController : MonoBehaviour
         {
             piano.localScale /= scale;
         }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ipConnection.oscOutIPAddress = textField.text;
+            ipConnection.ConnectOSCOut();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            dispatcher.SendOSCMessageDown();
+        }
+    }
+    // Numpad object methods
+    #region
+
+    // will make sure to concatenate string as more are selected
+    private void ChangeText(string newString)
+    {
+        if (ipAddress.text == "Enter IP Address" || ipAddress.text == fillerText)
+        {
+            ipAddress.text = newString;
+        }
+        else
+        {
+            ipAddress.text += newString;
+        }
     }
 
+    private void RemoveLastDigit()
+    {
+        {
+            ipAddress.text = fillerText;
+        }
+    }
+
+    // Update IP string with digit 7
+    public void Add7String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                ChangeText("7");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add8String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("8");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add9String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("9");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add4String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("4");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add5String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("5");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add6String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("6");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add1String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("1");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add2String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("2");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add3String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("3");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void AddPeriodString(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText(".");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+    public void Add0String(InteractableStateArgs obj)
+    {
+        if (limitSelection == false)
+        {
+            limitSelection = true;
+
+            if (obj.NewInteractableState == InteractableState.ActionState)
+            {
+                // update address
+                ChangeText("0");
+            }
+            // play coroutine to reset bool
+            StartCoroutine(ResetBool());
+        }
+    }
+
+    public void ClearText(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            // update address
+            RemoveLastDigit();
+        }
+    }
+
+    // Update IP Address to accurately reflect phone address
+    public void EnterIPAddress(InteractableStateArgs obj)
+    {
+        if (obj.NewInteractableState == InteractableState.ActionState)
+        {
+            // update address
+            ipConnection.oscOutIPAddress = ipAddress.text;
+            ipConnection.ConnectOSCOut();
+        }
+    }
+
+    #endregion
+
+    // for scaling piano scale
+    #region
     public void IncreasePianoScale(InteractableStateArgs obj)
     {
         if (limitSelection == false)
@@ -63,6 +299,7 @@ public class CustomUIController : MonoBehaviour
             }
         }
     }
+    #endregion
 
     private IEnumerator ResetBool()
     {
